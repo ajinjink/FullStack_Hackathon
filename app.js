27163a8525edd24cf019bf6209504c228b2c3476
Jina Kim <drjina02@gmail.com>
2023-12-23 16:39:48 +0900

    session from login to display my portfolios

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const path = require('path');
 
 const express = require('express');
+const session = require('express-session');
+const mysqlSession = require('express-mysql-session')(session);
 
+const db = require('./data/database');
 const authRoutes = require('./routes/auth');
 const portfolioRoutes = require('./routes/portfolio');
+
+const sessionStore = new mysqlSession({}, db);
 
 const app = express();
 
@@ -12,6 +17,13 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.use(session({ 
+    secret: 'super-secret', 
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore 
+  }));
 
 app.use(authRoutes);
 app.use(portfolioRoutes);
--- routes/portfolio.js
+++ routes/portfolio.js
@@ -2,17 +2,50 @@ const express = require('express');
 const db = require('../data/database');
 const router = express.Router();
 
-router.get('/main', function(req, res) {
+router.get('/main', async function(req, res) {
     const query = `
-    select id, title, userId, created_at, count
-    from post 
-    where id = userId
+        select post.*, users.name as user_name 
+        from post inner join users on post.userId = users.id
     `
-    res.render('list');
+    const [posts] = await db.query(query);
+
+    const postsData = posts.map(post => {
+        return {
+            ...post,
+            created_at: new Date(post.created_at).toLocaleDateString('ko-KR', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+            })
+        }
+    });
+
+    res.render('list', {posts: postsData});
 })
 
-router.get('/view', function(req,res){
-    res.render('view');
+router.get('/view/:postid', async function(req,res){
+    const query = `
+        select post.*, users.name as user_name 
+        from post inner join users on post.userId = users.id
+    `;
+    const [posts] = await db.query(query, [req.params.id]);
+
+    if (!posts || posts.length === 0) { // 일치하는 게시글 없음
+        return res.status(404).render('404');
+    }
+
+    const postData = {
+        ...posts[0], // posts[0]의 모든 속성 복사, 분해 후 나열
+        date: posts[0].created_at.toISOString(), // 기계를 위한 시간
+        human_date: posts[0].created_at.toLocaleDateString('ko-KR', { // 인간들이 볼 시간
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        })
+    }
+
+    res.render('edit', {post: postData});
 })
 
 module.exports = router;
